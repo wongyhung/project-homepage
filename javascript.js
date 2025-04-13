4b1857cce59721eea17c843f62b83f0a345ec871
Wong Yhung <wongyhung@gmail.com>
2025-04-13 16:34:13 +0800

    Add myWorkContainer animation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
 window.addEventListener("load", function () {
     document.querySelector('.headerbg').classList.add('skewed');
+    document.querySelector('.headerContainer').classList.add('loaded')
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const myWork = document.querySelector('.myWorkContainer');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                myWork.classList.add('loaded');
+                observer.unobserve(myWork); // stop observing after it loads once
+            }
+        });
+    }, {
+        threshold: 0.3 // Trigger when 30% of the element is visible
+    });
+
+    observer.observe(myWork);
 });
